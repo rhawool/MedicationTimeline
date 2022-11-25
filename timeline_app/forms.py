84f9968627b222544231f162aa4e32dda455372e
Rahul Adsule <rahul.adsule@yash.com>
2022-11-25 19:30:20 +0530

    third commit

--- timeline_app/forms.py
+++ timeline_app/forms.py
@@ -3,12 +3,6 @@ from datetime import datetime
 from timeline_app.models import IntakeEntry
 
 
-#
-# class AddMedicineForm(ModelForm):
-#     class Meta:
-#         model = Medicine
-#         fields = '__all__'
-#
 class AddEntryForm(ModelForm):
     class Meta:
         model = IntakeEntry
